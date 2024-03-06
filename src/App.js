a07ec9bf9945893040cf25052fd447744915e9c3
Vinish-Amalraj <vinivinish03@gmail.com>
2024-03-06 12:13:25 +0530

    First-update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Nav from "./Components/Nav/Nav";
+import Login from "./Components/Login/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MovieDetails from "./Components/MovieDetails/MovieDetails";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Movie from "./Components/Movies/Movie";
 function App() {
+  const api_key = "737f42de283d5dbfc26b1164d552e787";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          <Route
+            path="/movies/:id"
+            element={
+              <>
+                <Nav />
+                <MovieDetails api_key={api_key} />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Nav />
+                <Header />
+                <Movie
+                  url={`https://api.themoviedb.org/3/trending/movie/week?api_key=${api_key}`}
+                  title="Trending"
+                />
+                <Movie
+                  url={`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`}
+                  title="Popular"
+                />
+                <Movie
+                  url={`https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=en-US&page=1`}
+                  title="Top Rated"
+                />
+
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
